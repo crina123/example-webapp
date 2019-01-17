9fd196e1b20742736ab82ec0873251e1e3e68122
crina123 <you@example.com>
2019-01-17 02:07:04 +0100

    adding

--- run-stack.sh
+++ run-stack.sh
@@ -7,6 +7,6 @@ aws cloudformation $ACTION \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameters \
     ParameterKey=DockerImage,ParameterValue=191414186247.dkr.ecr.us-east-2.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
-    ParameterKey=VPC,ParameterValue=vpc-2736394f
+    ParameterKey=VPC,ParameterValue=vpc-2736394f \
     ParameterKey=Cluster,ParameterValue=default2 \
     ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-east-2:191414186247:loadbalancer/app/production-website/b78d88e35ba11784
