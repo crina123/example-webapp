48f76ec85452465fe4572f63be8586c3a49c9177
crina123 <you@example.com>
2019-01-17 03:06:46 +0100

    adding variable arn listener

--- run-stack.sh
+++ run-stack.sh
@@ -9,4 +9,4 @@ aws cloudformation $ACTION \
     ParameterKey=DockerImage,ParameterValue=191414186247.dkr.ecr.us-east-2.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
     ParameterKey=VPC,ParameterValue=vpc-2736394f \
     ParameterKey=Cluster,ParameterValue=default2 \
-    ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-east-2:191414186247:listener/app/production-website2/9d5f383fe8c7df05/a4e7235acbd84f27
+    ParameterKey=Listener,ParameterValue=$ALB_LISTENER_ARN
